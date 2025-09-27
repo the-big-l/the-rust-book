943b6704f5adab7a38774197e5617b80c6bd1ade
Leo Salat <leo.salat@gmail.com>
2025-09-26 17:18:23 -0700

    functions part 2 of 2

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,23 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function(5);
+    print_lable_measurement(5, 'm');
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {}", x);
+fn print_lable_measurement(value: i32, lable: char) {
+    println!("The measurement is: {}{}", value, lable);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
